df64d30870543938592a2826b65cae42974ac708
akshay kapoor <akshaykapoor347@gmail.com>
2018-07-02 11:38:03 -0400

    Update AkshayBreastCancerAnalysis.py
    Please refer the AkshayBreastCancerAnalysis.ipynb notebook for better explanation, I have made this file as there were problems in importing functions from .ipynb notebook. So file is just made for re usability

--- AkshayBreastCancerAnalysis.py
+++ AkshayBreastCancerAnalysis.py
@@ -1,4 +1,7 @@
 
+#Author Akshay Kapoor 
+
+#Please refer the AkshayBreastCancerAnalysis.ipynb notebook for better explanation, I have made this file as there were problems in importing functions from .ipynb notebook. So file is just made for re usability  
 # coding: utf-8
 
 # # Breast cancer detection
@@ -36,9 +39,7 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt 
 import seaborn as sns 
-get_ipython().run_line_magic('matplotlib', 'inline')
 
-#scikit learn libraries
 from sklearn.model_selection import train_test_split
 from sklearn.cross_validation import KFold
 from sklearn.linear_model import LogisticRegression
@@ -47,149 +48,62 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
 
 
-# ## Let us learn more about the data 
-
-# In[4]:
-
-
-df = pd.read_csv("AkshayData.csv")
-
-
-# In[5]:
-
-
-df.head()
-
-
-# In[6]:
-
-
-df.info()
+def data_analysis():
+    df = pd.read_csv("AkshayData.csv")
+    df.head()
+    df.info()
+    df.drop('Unnamed: 32', axis  = 1, inplace=True)
+    df.drop('id', axis = 1, inplace= True)
+    df['diagnosis'] = df['diagnosis'].map({'M':1,'B':0})
+    df.describe()
+    sns.countplot(df['diagnosis'])
+    df.columns
+    first = list(df.columns[1:10])
+    train, test = train_test_split(df, test_size = 0.25)
+  
+    second = list(df.columns[11:21])
+    third =  list(df.columns[21:30])
+    
 
 
-# In[7]:
+
+    corr1 = df[first].corr()
+    #Let us visualize with a heatmap
+    plt.figure(figsize=(14,10))
+    sns.heatmap(corr1, cmap='coolwarm', xticklabels = first,  yticklabels = first, annot=True)
 
 
-#We can see Unnamed:32 has all null values hence we cannot use this column for our analysis and id will also be of no use for analysis
-df.drop('Unnamed: 32', axis  = 1, inplace=True)
-df.drop('id', axis = 1, inplace= True)
+    # **We can see that radius, perimeter and area are highly correlated as seen from the heatmap.** <br>
+    # **Also compactness_mean, concavepoint_mean and concavity_mean are highly correlated**
 
+    #Let us perform analysis on the mean features
 
-# In[8]:
+    melign = df[df['diagnosis'] == 1][first]
+    bening = df[df['diagnosis'] == 0][first]
 
+    melign.columns
 
-#Let us convert 'Malign' and 'Benign' to 1 and 0 respectively so it will be easier for analysis
+    for columns in melign.columns:
+        plt.figure()
+        sns.distplot(melign[columns], kde=False, rug= True)
+        sns.distplot(bening[columns], kde=False, rug= True)
+        sns.distplot
+        plt.tight_layout()
 
-df['diagnosis'] = df['diagnosis'].map({'M':1,'B':0})
-
-
-# In[9]:
-
-
-df.describe()
-
-
-# ## Performing Exploratory Data Analysis
-# 
-
-# In[10]:
-
-
-sns.countplot(df['diagnosis'])
-
-
-# We can see there are almost double number patients with benign cancer
-
-# In[11]:
-
-
-df.columns
-
-
-# In[12]:
-
-
-#The mean, standard error, and "worst" or largest (mean of the three largest values) of these features were computed for each image,resulting in 30 features.
-#For instance, field 3 is Mean Radius, field 13 is Radius SE, field 23 is Worst Radius.
-#more info at https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.names
-
-first = list(df.columns[1:10])
-second = list(df.columns[11:21])
-third =  list(df.columns[21:30])
-
-
-# In[13]:
-
-
-#Let us find the correlation between different attributes
-corr1 = df[first].corr()
-
-
-# In[14]:
-
-
-#Let us visualize with a heatmap
-plt.figure(figsize=(14,10))
-sns.heatmap(corr1, cmap='coolwarm', xticklabels = first,  yticklabels = first, annot=True)
-
-
-# **We can see that radius, perimeter and area are highly correlated as seen from the heatmap.** <br>
-# **Also compactness_mean, concavepoint_mean and concavity_mean are highly correlated**
-# 
-
-# In[15]:
-
-
-#Let us perform analysis on the mean features
-
-melign = df[df['diagnosis'] == 1][first]
-bening = df[df['diagnosis'] == 0][first]
-
-
-# In[16]:
-
-
-melign.columns
-
-
-# In[17]:
-
-
-for columns in melign.columns:
-    plt.figure()
-    sns.distplot(melign[columns], kde=False, rug= True)
-    sns.distplot(bening[columns], kde=False, rug= True)
-    sns.distplot
-plt.tight_layout()
-
+        
 
 # We can see that the mean values of perimeter, area, concavity, compactness, radius and concave points can be used for classification as these parameters show a correlation. <br>
 # While parameters such as smoothness, symmetry, fractual dimension and texture don't show much seperation and is of not much use for classification.
-# 
+    color_function = {0: "green", 1: "red"}
+    colors = df["diagnosis"].map(lambda x: color_function.get(x))
 
-# In[18]:
-
-
-color_function = {0: "green", 1: "red"}
-colors = df["diagnosis"].map(lambda x: color_function.get(x))
-
-pd.plotting.scatter_matrix(df[first], c=colors, alpha = 0.4, figsize = (15, 15));
-
-
-# **Using a scatter matrix we can see a well seperation of malign and benign cancer with green points indication benign cancer cells and red points indicating malign cancer cells.**
-# 
-
-# ## Machine learning
-# 
-
-# In[19]:
-
-
-#We divide the data into Training and test set 
-train, test = train_test_split(df, test_size = 0.25)
-
-
-# In[20]:
+    pd.plotting.scatter_matrix(df[first], c=colors, alpha = 0.4, figsize = (15, 15))
+  #
+  
+    # **Using a scatter matrix we can see a well seperation of malign and benign cancer with green points indication benign cancer cells and red points indicating malign cancer cells.**
+ 
+    #We divide the data into Training and test set 
+    
 
 
 # I have created a function to perform k folds cross validation which helps in obtaining a better insight to test the accuracy of the model
@@ -220,78 +134,56 @@ def classification_model(model, data, predictors, outcome):
   model.fit(data[predictors],data[outcome]) 
 
 
-# In[21]:
-
+def mac_classification():
 
 #Using Logistic regression on the top five features
 #more info at https://en.wikipedia.org/wiki/Logistic_regression
-
-predictor_var = ['radius_mean','perimeter_mean','area_mean','compactness_mean','concave points_mean']
-outcome_var='diagnosis'
-model=LogisticRegression()
-classification_model(model,train,predictor_var,outcome_var)
-
-
-# In[22]:
-
+    df = pd.read_csv("AkshayData.csv")
+    df.drop('Unnamed: 32', axis  = 1, inplace=True)
+    df.drop('id', axis = 1, inplace= True)
+    df['diagnosis'] = df['diagnosis'].map({'M':1,'B':0})
+    first = list(df.columns[1:10])
+    train, test = train_test_split(df, test_size = 0.25)
+  
+    
+    predictor_var = ['radius_mean','perimeter_mean','area_mean','compactness_mean','concave points_mean']
+    outcome_var='diagnosis'
+    model=LogisticRegression()
+    classification_model(model,train,predictor_var,outcome_var)
 
 #Let us check the accuracy on test data
-classification_model(model, test,predictor_var,outcome_var)
-
-
-# In[23]:
-
+    classification_model(model, test,predictor_var,outcome_var)
 
 #Let us try to classify using a decision tree classifier 
-predictor_var = ['radius_mean','perimeter_mean','area_mean','compactness_mean','concave points_mean']
-model = DecisionTreeClassifier()
-classification_model(model,train,predictor_var,outcome_var)
+    predictor_var = ['radius_mean','perimeter_mean','area_mean','compactness_mean','concave points_mean']
+    model = DecisionTreeClassifier()
+    classification_model(model,train,predictor_var,outcome_var)
 
 
 # We are getting 100% accuracy! Is it overfitting let us try it on test data
-# 
-
-# In[24]:
-
-
-classification_model(model, test,predictor_var,outcome_var)
-
-
+    classification_model(model, test,predictor_var,outcome_var)
 # Let us try using random forest
 
-# In[25]:
-
-
-predictor_var = first
-model = RandomForestClassifier()
-classification_model(model, train,predictor_var,outcome_var)
-
-
-# In[26]:
-
+    predictor_var = first
+    model = RandomForestClassifier()
+    classification_model(model, train,predictor_var,outcome_var)
 
 #Let us find the most important features used for classification model
 
-featimp = pd.Series(model.feature_importances_, index=predictor_var).sort_values(ascending=False)
-print(featimp)
-
-
-# In[27]:
-
-
-predictor_var = ['concave points_mean','area_mean','radius_mean','perimeter_mean','concavity_mean']
-model = RandomForestClassifier()
-classification_model(model,train,predictor_var,outcome_var)
-
-
-# In[28]:
-
+    featimp = pd.Series(model.feature_importances_, index=predictor_var).sort_values(ascending=False)
+    print(featimp)
+    predictor_var = ['concave points_mean','area_mean','radius_mean','perimeter_mean','concavity_mean']
+    model = RandomForestClassifier()
+    classification_model(model,train,predictor_var,outcome_var)
 
 # I think we get a better prediction with all the features now let us try it on test data!
-predictor_var = first
-model = RandomForestClassifier()
-classification_model(model, test,predictor_var,outcome_var)
+    predictor_var = first
+    model = RandomForestClassifier()
+    classification_model(model, test,predictor_var,outcome_var)
 
+if __name__ == "__main__":
+    data_analysis()
+    mac_classification()
 
 # ## Conclusion
 # 
